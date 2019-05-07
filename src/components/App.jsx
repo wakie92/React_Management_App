2637d7087df21c2020401ae73459eb0334af607e
Changhun Lee <wakie9292@gmail.com>
2019-05-07 20:40:25 +0900

    Router setting

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {Route, Switch} from 'react-router-dom'
+import {
+  Home,
+  WorkersList
+} from 'pages'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Switch> 
+          <Route exact path = '/' component = {Home} />
+          <Route exact path = '/workerslist' component = {WorkersList} />
+        </Switch>
+      </>
+    );
+  }
+}
+export default App;
