1b3236a802e0ae979aeca42dc66dfbffea74179c
Changhun Lee <wakie9292@gmail.com>
2019-05-07 20:41:46 +0900

    WorkersList componet

---
+++ src/components/WorkersList/WorkersList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './WorkersList.module.scss';
+
+const WorkersList = () => {
+
+  return (
+    <div>WorkersList</div>
+  )
+}
+export default WorkersList;
