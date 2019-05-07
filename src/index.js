e5979d4e2015995d40dd88803d3a5276e687aa88
Changhun Lee <wakie9292@gmail.com>
2019-05-07 20:39:36 +0900

    index Root Changed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Root from './Root';
+import Root from 'Root';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
