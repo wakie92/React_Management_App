b7aa8a89abdaa8d02a3e4102c72f47a37a6f046e
Changhun Lee <wakie9292@gmail.com>
2019-05-07 20:43:35 +0900

    workerList page

---
+++ src/pages/workersList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import WorkersList from 'components/WorkersList';
+
+const workersList =  () => {
+  return (
+    <>
+      <WorkersList/>
+    </>
+  )
+}
+export default workersList;
