1a5248093b5a8c368f3d47e0dc94e4848b6d286c
Changhun Lee <wakie9292@gmail.com>
2019-05-09 02:53:20 +0900

    WorkersList design

--- src/components/WorkersList/WorkersList.jsx
+++ src/components/WorkersList/WorkersList.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import classes from './WorkersList.module.scss';
-
+import Aside from 'components/UI/Aside'
+import Main from 'components/UI/Main';
 const WorkersList = () => {
 
   return (
     <div className = {classes.Layout}>
+      <Aside/>
+      <Main/>
     </div>
   )
 }
