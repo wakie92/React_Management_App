19c9db8c91b99925301ac28794418b10743db511
Changhun Lee <wakie9292@gmail.com>
2019-05-09 01:09:43 +0900

    Aside setting

---
+++ src/components/UI/Aside/Aside.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './WorkersListAside.module.scss';
+
+const Aside = () => {
+
+  return (
+    <aside>
+      Aside
+    </aside>
+  )
+}
+export default Aside;
+
+
