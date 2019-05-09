52639847a4a8d2d1424a991298412647c96b1c9a
mkkyung <cng9323@gmail.com>
2019-05-09 16:19:14 +0900

    login publishing

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,25 +1,31 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import classes from './Home.module.scss';
 
 const Home = () => {
   return (
-    <Fragment>
+    <div className={classes.Home_box}>
       <div className={classes.Home_outbox}>
         <div className={classes.Home_input}>
-          <ul className={classes.Home_message}>
-            <li>Welcome Back</li>
-          </ul>
-          <ul>
-            <li>계정</li>
-            <input type="text" />
-            <li>비밀번호</li>
-            <input type="password" />
-          </ul>
-          <div>버튼자리</div>
+          <div className={classes.Home_content}>
+            <ul>
+              <li className={classes.Home_message}>Welcome Back</li>
+            </ul>
+            <ul>
+              <li className={classes.Home_items}>Email Address</li>
+            </ul>
+            <input className={classes.Home_input_tag} type="text" autoFocus placeholder="Email Address" />
+            <ul>
+              <li className={classes.Home_items}>Password</li>
+            </ul>
+            <input className={classes.Home_input_tag} type="password" placeholder="Password"/>
+            <div>버튼자리</div>
+          </div>
         </div>
-        <div className={classes.Picture}>abcd</div>
+        <div className={classes.Picture}>
+          <img src="https://dspncdn.com/a1/media/236x/ed/d7/da/edd7dad1071f3c982dd9efab0b68af41.jpg"></img>
+        </div>
       </div>
-    </Fragment>
+    </div>
   )
 }
 
