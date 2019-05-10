ba3bb5e147c40ec249c3df2edba375fbbe68c19d
Changhun Lee <wakie9292@gmail.com>
2019-05-10 22:06:27 +0900

    directory Changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as Home} from './home';
 export { default as WorkersList} from './workersList';
 export { default as WorkerInfoDetail} from './workerInfoDetail';
-export { default as WorkerInfoAttendance} from './workerInfoAttendance'
+export { default as WorkerInfoAttendance} from './workerInfoAttendance';
