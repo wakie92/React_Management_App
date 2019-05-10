dd3f6a3dcb2bb528eda7f692c87bb0fb3776f139
Changhun Lee <wakie9292@gmail.com>
2019-05-10 21:24:05 +0900

    Add pages

---
+++ src/pages/workerInfoAttendance.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from 'components/UI/Layout'
+import WorkerInfoAttComponent from 'components/WorkerInfoAttendance'
+const WorkerInfoAttendance = () => {
+  return (
+    <Layout>
+      <WorkerInfoAttComponent/>
+    </Layout>
+  )
+}
+export default WorkerInfoAttendance;
---
+++ src/pages/workerInfoDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from 'components/UI/Layout'
+import WorkerInfoComponent from 'components/WorkerInfoDetail';
+
+const workerInfoDetail =  () => {
+  return (
+    <>
+      <Layout>
+        <WorkerInfoComponent/>
+      </Layout>
+    </>
+  )
+}
+export default workerInfoDetail;
