6df9de241a5b098360b4a4f94e98f01434a00968
Changhun Lee <wakie9292@gmail.com>
2019-05-10 21:23:21 +0900

    Attendance Info Wrapper component

---
+++ src/components/WorkerInfoAttendance/WorkerInfoAttendance.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainWorkerInfoAttendance from 'components/UI/Main/MainWorkerInfoAttendance';
+import MainContentsSubHeader from 'components/UI/Main/MainContentsSubHeader';
+
+const WorkerInfoAttendance = () => {
+  console.log('3')
+  return (
+    <>
+      <MainContentsSubHeader/>
+      <MainWorkerInfoAttendance/>
+    </>
+  )
+}
+export default WorkerInfoAttendance;
