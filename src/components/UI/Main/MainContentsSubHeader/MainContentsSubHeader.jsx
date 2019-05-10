6e43d2a834269c1a756d5e0766ed3bd1795be528
Changhun Lee <wakie9292@gmail.com>
2019-05-10 21:21:11 +0900

    Sub-Header for currentPage

--- src/components/UI/Main/MainContentsSubHeader/MainContentsSubHeader.jsx
+++ src/components/UI/Main/MainContentsSubHeader/MainContentsSubHeader.jsx
@@ -4,7 +4,14 @@ import classes from './MainContentsSubHeader.module.scss';
 const MainContentsSubHeader = () => {
 
   return (
-    <div className = {classes.Line}></div>
+    <div className = {classes.SubHeaderWrapper}>
+      <div className = {classes.CurPage}>
+        <span>신상정보</span> 
+        <span>근태정보</span>
+      </div>
+      <div className = {classes.Line}>
+      </div>
+    </div>
   )
 }
 export default MainContentsSubHeader;
