d2edfb48194091dd80bbbac51442d5fa5b3f7b6f
Changhun Lee <wakie9292@gmail.com>
2019-05-10 21:23:50 +0900

    Detail Info Wrapper component

---
+++ src/components/WorkerInfoDetail/WorkerInfoDetail.jsx
@@ -0,0 +1,13 @@
+import React from './WorkerInfoDetail';
+import MainWorkerDetailInfo from 'components/UI/Main/MainWorkerDetailInfo';
+import MainContentsSubHeader from 'components/UI/Main/MainContentsSubHeader';
+const WorkerInfo = () => {
+
+  return (
+    <>
+      <MainContentsSubHeader/>
+      <MainWorkerDetailInfo/>
+    </>
+  )
+}
+export default WorkerInfo;
