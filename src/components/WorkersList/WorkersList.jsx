53ecfe3bb3621dc1eb3a833889eb5eaef5ff39f2
mkkyung <cng9323@gmail.com>
2019-05-14 16:39:49 +0900

    0514

--- src/components/WorkersList/WorkersList.jsx
+++ src/components/WorkersList/WorkersList.jsx
@@ -5,61 +5,49 @@ const WorkersList = () => {
 
   return (
     <>
-      <div className={classes.Category_bar}>
-        <div className={classes.Category_ID}>사원번호</div>
-        <div className={classes.Category_name}>성명</div>
-        <div className={classes.Category_grade}>직급</div>
-        <div className={classes.Category_phone}>전화번호</div>
-        <div className={classes.Category_date}>입사일자</div>
-      </div>
-      <div className={classes.Workerlist_bar}>
-        <div className={classes.Content_ID}>12345</div>
-        <div className={classes.Content_name}>홍길동</div>
-        <div className={classes.Content_grade}>대리</div>
-        <div className={classes.Content_phone}>010-2723-9323</div>
-        <div className={classes.Content_date}>2019-03-01</div>
-      </div>
-      <div className={classes.Workerlist_bar}>
-        <div className={classes.Content_ID}>12345</div>
-        <div className={classes.Content_name}>홍길동</div>
-        <div className={classes.Content_grade}>대리</div>
-        <div className={classes.Content_phone}>010-2723-9323</div>
-        <div className={classes.Content_date}>2019-03-01</div>
-      </div>
-      <div className={classes.Workerlist_bar}>
-        <div className={classes.Content_ID}>12345</div>
-        <div className={classes.Content_name}>홍길동</div>
-        <div className={classes.Content_grade}>대리</div>
-        <div className={classes.Content_phone}>010-2723-9323</div>
-        <div className={classes.Content_date}>2019-03-01</div>
-      </div>
-      <div className={classes.Workerlist_bar}>
-        <div className={classes.Content_ID}>12345</div>
-        <div className={classes.Content_name}>홍길동</div>
-        <div className={classes.Content_grade}>대리</div>
-        <div className={classes.Content_phone}>010-2723-9323</div>
-        <div className={classes.Content_date}>2019-03-01</div>
-      </div>
-      <div className={classes.Workerlist_bar}>
-        <div className={classes.Content_ID}>12345</div>
-        <div className={classes.Content_name}>홍길동</div>
-        <div className={classes.Content_grade}>대리</div>
-        <div className={classes.Content_phone}>010-2723-9323</div>
-        <div className={classes.Content_date}>2019-03-01</div>
-      </div>
-      <div className={classes.Workerlist_bar}>
-        <div className={classes.Content_ID}>12345</div>
-        <div className={classes.Content_name}>홍길동</div>
-        <div className={classes.Content_grade}>대리</div>
-        <div className={classes.Content_phone}>010-2723-9323</div>
-        <div className={classes.Content_date}>2019-03-01</div>
-      </div>
-      <div className={classes.Workerlist_bar}>
-        <div className={classes.Content_ID}>12345</div>
-        <div className={classes.Content_name}>홍길동</div>
-        <div className={classes.Content_grade}>대리</div>
-        <div className={classes.Content_phone}>010-2723-9323</div>
-        <div className={classes.Content_date}>2019-03-01</div>
+      <div className={classes.Workerlist_layout}>
+        <div className={classes.Category_bar}>
+          <div className={classes.Category_ID}>사원번호</div>
+          <div className={classes.Category_name}>성명</div>
+          <div className={classes.Category_grade}>직급</div>
+          <div className={classes.Category_phone}>전화번호</div>
+          <div className={classes.Category_date}>입사일자</div>
+        </div>
+        <div className={classes.Workerlist_bar}>
+          <div className={classes.Content_ID}>12345</div>
+          <div className={classes.Content_name}>홍길동</div>
+          <div className={classes.Content_grade}>대리</div>
+          <div className={classes.Content_phone}>010-2723-9323</div>
+          <div className={classes.Content_date}>2019-03-01</div>
+        </div>
+        <div className={classes.Workerlist_bar}>
+          <div className={classes.Content_ID}>12345</div>
+          <div className={classes.Content_name}>홍길동</div>
+          <div className={classes.Content_grade}>대리</div>
+          <div className={classes.Content_phone}>010-2723-9323</div>
+          <div className={classes.Content_date}>2019-03-01</div>
+        </div>
+        <div className={classes.Workerlist_bar}>
+          <div className={classes.Content_ID}>12345</div>
+          <div className={classes.Content_name}>홍길동</div>
+          <div className={classes.Content_grade}>대리</div>
+          <div className={classes.Content_phone}>010-2723-9323</div>
+          <div className={classes.Content_date}>2019-03-01</div>
+        </div>
+        <div className={classes.Workerlist_bar}>
+          <div className={classes.Content_ID}>12345</div>
+          <div className={classes.Content_name}>홍길동</div>
+          <div className={classes.Content_grade}>대리</div>
+          <div className={classes.Content_phone}>010-2723-9323</div>
+          <div className={classes.Content_date}>2019-03-01</div>
+        </div>
+        <div className={classes.Workerlist_bar}>
+          <div className={classes.Content_ID}>12345</div>
+          <div className={classes.Content_name}>홍길동</div>
+          <div className={classes.Content_grade}>대리</div>
+          <div className={classes.Content_phone}>010-2723-9323</div>
+          <div className={classes.Content_date}>2019-03-01</div>
+        </div>        
       </div>
     </>
   )
