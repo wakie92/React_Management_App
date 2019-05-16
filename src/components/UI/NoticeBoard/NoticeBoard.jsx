54101a4ac0efdb1f8cc962f4a2ab693de5174233
Changhun Lee <wakie9292@gmail.com>
2019-05-16 19:38:25 +0900

    Notieve Board Layout Page

---
+++ src/components/UI/NoticeBoard/NoticeBoard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './NoticeBoard.module.scss';
+
+const NoticeBoard = () => {
+
+  return (
+    <div>NoticeBoard</div>
+  )
+}
+export default NoticeBoard;
