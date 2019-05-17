b819715cf53c09e24d848d8ed615f38ed95ba08f
Changhun Lee <wakie9292@gmail.com>
2019-05-17 23:49:06 +0900

    deleted MainContentsHeader in Layout

--- src/components/UI/Layout/Layout.jsx
+++ src/components/UI/Layout/Layout.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import classes from './Layout.module.scss';
 import Aside from 'components/UI/Aside'
 import Main from 'components/UI/Main';
-import MainContentsHeader from 'components/UI/Main/MainContentsHeader';
 
 const Layout = ({children,pageType}) => {
   return (
@@ -10,7 +9,6 @@ const Layout = ({children,pageType}) => {
       <div className = {classes.Layout}>
           <Aside/>
           <Main>
-            <MainContentsHeader/>
             {children}
           </Main>
       </div>
