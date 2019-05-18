54c7109ee8672220b8baa89665984d34c0ca4e39
Changhun Lee <wakie9292@gmail.com>
2019-05-18 17:45:39 +0900

    Add MainHeader Component

--- src/components/UI/Main/Main.jsx
+++ src/components/UI/Main/Main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import classes from './Main.module.scss';
-
+import MainHeader from './MainHeader';
 const Main = ({children}) => {
   return (
     <main>
       <section className = {classes.SectionHeader}>
-        header
+        <MainHeader/>
       </section>
       <section className = {classes.SectionContents}>
         {children}
