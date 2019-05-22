543054b8819cfb99e591ef69099c2705debe3b75
Changhun Lee <wakie9292@gmail.com>
2019-05-22 19:35:35 +0900

    Adjusted redux sturcture

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import App from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configure from 'store/configure';
+
+const store = configure();
+
 function Root() {
   return (
+    <Provider store = {store} >
       <BrowserRouter>
         <App/>
       </BrowserRouter>
+      </Provider>
   );
 }
 
