3bb5161fc7b759917a51343f0bc87a43525f9b18
Changhun Lee <wakie9292@gmail.com>
2019-06-14 22:17:58 +0900

    add props

--- front_management_app/src/pages/notice.tsx
+++ front_management_app/src/pages/notice.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Layout from 'components/UI/Layout';
 import Notice from 'components/Notice';
+import NoticeData from 'libs/boardData.json';
 
 const notice: React.FC = ({match}:any) => {
-  console.log(match);
   return (
     <Layout pageType = 'Notice'>
-      <Notice/>
+      <Notice NoticeData = {NoticeData} matchData = {match}/>
     </Layout>
   )
 }
