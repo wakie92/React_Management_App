0d7134f391b7882259fa34a736a098600d99a30e
Changhun Lee <wakie9292@gmail.com>
2019-06-17 22:28:27 +0900

    uploadComponent with antd

---
+++ front_management_app/src/components/UI/EditorComponent/UploadComponent/UploadComponent.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import classes from './UploadCompoent.module.scss';
+import { Upload, Button, Icon } from 'antd';
+
+const UploadCompoent: React.FC = () => {
+  const fileList = [
+    {
+      uid: '-1',
+      name: 'xxx.png',
+      status: 'done',
+      url:
+        'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+      thumbUrl:
+        'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    },
+    {
+      uid: '-2',
+      name: 'yyy.png',
+      status: 'done',
+      url:
+        'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+      thumbUrl:
+        'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    },
+  ];
+  const props = {
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    listType: 'picture',
+    defaultFileList: [...fileList],
+  };
+
+  return (
+    <div>
+      <Upload>
+        {/* <Upload {...props}> */}
+        <Button>
+          <Icon type="upload" /> Upload
+        </Button>
+      </Upload>
+    </div>
+  );
+};
+export default UploadCompoent;
