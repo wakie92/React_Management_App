2702154fe2f3a94e6faef5b87d1d9d75411e1cae
Changhun Lee <wakie9292@gmail.com>
2019-06-24 00:09:05 +0900

    removed faceData

--- front_management_app/src/pages/workersList.tsx
+++ front_management_app/src/pages/workersList.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import WorkersListContainer from 'containers/WorkersListContainer';
 import Layout from 'components/UI/Layout';
 import MainContentsHeader from 'components/UI/Main/MainContentsHeader';
-import workers from 'libs/staffInfo.json';
 
 const workersList: React.FC =  () => {
   return (
     <>
       <Layout pageType = 'List'>
         <MainContentsHeader/>
-        <WorkersListContainer staffList = {workers}/>
+        <WorkersListContainer />
       </Layout>
     </>
   )
