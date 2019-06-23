35e85663649c573e5f91d3777e54f764766675d1
Changhun Lee <wakie9292@gmail.com>
2019-06-24 00:10:47 +0900

    Add ul tag

--- front_management_app/src/components/WorkersList/WorkersList.tsx
+++ front_management_app/src/components/WorkersList/WorkersList.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import classes from './WorkersList.module.scss';
 import ListInfoContainer from 'containers/WorkerInfoContainer';
+import { WorkerInfo } from 'store/modules/workers';
 
 interface WorkerListProps {
-  staffList: any;
+  staffList: WorkerInfo[];
+  count: number;
+  handleCount: () => void;
 }
 
-const WorkersList: React.FC<WorkerListProps> = ({ staffList }) => {
-  const listOfStaff: React.FC = staffList.map((staff: any) => {
-    return <ListInfoContainer {...staff} key={staff.id} />;
-  });
+const WorkersList: React.FC<WorkerListProps> = ({
+  staffList,
+  count,
+  handleCount,
+}) => {
   return (
     <>
       <div className={classes.Workerlist_layout}>
+        <span onClick={handleCount}>{count}</span>
         <div className={classes.Category_bar}>
           <div className={classes.Category_name}>성명</div>
           <div className={classes.Category_grade}>직급</div>
@@ -20,7 +25,11 @@ const WorkersList: React.FC<WorkerListProps> = ({ staffList }) => {
           <div className={classes.Category_phone}>전화번호</div>
           <div className={classes.Category_date}>입사일자</div>
         </div>
-        {listOfStaff}
+        <ul>
+          {staffList.map(staff => {
+            return <ListInfoContainer staff = {staff} key={staff.id} />;
+          })}
+        </ul>
       </div>
     </>
   );
